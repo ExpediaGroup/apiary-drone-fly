43189b49dec50d8b19e7589d45fcfc9bfbd85f2e
Javier Sánchez Beltrán <36443689+javsanbel2@users.noreply.github.com>
2025-05-23 15:14:28 +0200

    Add Datadog Metrics in drone fly (#9)
    * add datadog metrics
    
    * forget this
    
    * testing with new openmetrics
    
    * just tf
    
    * changelog

--- variables.tf
+++ variables.tf
@@ -103,3 +103,21 @@ variable "service_account_name" {
   type        = string
   default     = ""
 }
+
+variable "datadog_metrics_enabled" {
+  description = "Datadog namespace to use as prefix when deploying metrics."
+  type        = bool
+  default     = false
+}
+
+variable "datadog_namespace" {
+  description = "Datadog namespace to use as prefix when deploying metrics."
+  type        = string
+  default     = "dronefly"
+}
+
+variable "datadog_metric_filters" {
+  description = "Datadog metrics key used to filter metrics sent to Datadog."
+  type        = list(string)
+  default     = []
+}
